fff4d60f2b7f2c3a4abcbbe48dad53a8356b84dc
Joaquin Tiesi <joaquin.tiesi@gmail.com>
2021-04-07 20:13:11 -0300

    php?

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
-<?php header( ‘Location: /html_file_name.html’ ) ; ?>
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Menu ejercicios del curso</title>
+  </head>
+  <body>
+    <h1>Indice de ejercicios de la materia Laboratorio III</h1>
+    <ul>
+      <li><a href="./Html/index.html">Html</a></li>
+      <li><a href="#">Css</a></li>
+      <li><a href="#">Jscript</a></li>
+      <li><a href="#">Esoeciales</a></li>
+      <li><a href="#">Php</a></li>
+    </ul>
+
+  </body>
+</html>
