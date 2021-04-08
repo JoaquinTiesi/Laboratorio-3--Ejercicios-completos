f7c2837641db5e3a50093bd01b216c86b901e9f9
Joaquin Tiesi <joaquin.tiesi@gmail.com>
2021-04-08 16:26:54 -0300

    links

--- index.php
+++ index.php
@@ -8,10 +8,11 @@
     <h1>Indice de ejercicios de la materia Laboratorio III</h1>
     <ul>
       <li><a href="./Html/index.html">Html</a></li>
-      <li><a href="./Html/index.html">Css</a></li>
-      <li><a href="./Html/index.html">Jscript</a></li>
-      <li><a href="./Html/index.html">Especiales</a></li>
-      <li><a href="./Html/index.html">Php</a></li>
+      <li><a href="./Css/index.html">Css</a></li>
+      <li><a href="./Jscript/index.html">Jscript</a></li>
+      <li><a href="./Especiales/index.html">Especiales</a></li>
+      <li><a href="./Php/index.html">Php</a></li>
+      <li><a href="./Otros/index.html">Otros</a></li>
     </ul>
 
   </body>
