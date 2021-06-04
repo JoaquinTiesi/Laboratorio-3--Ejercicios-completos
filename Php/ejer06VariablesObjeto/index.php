22962b3181a579ab1d9b2afa0a6273512f850a89
Joaquin Tiesi <joaquin.tiesi@gmail.com>
2021-06-04 11:39:30 -0300

    php1.1

--- Php/ejer06VariablesObjeto/index.php
+++ Php/ejer06VariablesObjeto/index.php
@@ -119,6 +119,16 @@
          <h1>Tipo de $objRenglonPedido: <?php echo gettype($objRenglonPedido) ?></h1>
          <h1>Definamos arreglo de pedidos:</h1>
          <h2><span style="color:blue">#$renglonesPedido</span> </h2>
+         cp001  Jaguel  800 gr  2000  2
+         cp002  Jaguel  400 gr  1400  10
+         <br><br>
+         <h2>Cantidad de renglones:</h2>
+         <?php echo sizeof($renglonesPedido) ?>
+         <br>
+         <h1>Procuccion de un JSON jsnRenglones:</h1>
+         <br>
+         <?php $jsonRenglonesPedido = json_encode($renglonesPedido);
+         echo $jsonRenglonesPedido ?>
       </div>
       <div class="anterior">
         <a href="../index.html">Volver a la pagina anterior.</a>
