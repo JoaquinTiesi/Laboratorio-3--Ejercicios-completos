a7ccb7c92638a97fbaae00a4e898108ee1ade027
Joaquin Tiesi <joaquin.tiesi@gmail.com>
2021-06-18 17:27:03 -0300

    php2.6.3.2

--- Php/ejer24BDListaOrdenaFiltra/PHPData.php
+++ Php/ejer24BDListaOrdenaFiltra/PHPData.php
@@ -14,29 +14,42 @@ $sql=$sql."foto LIKE ? and ";
 $sql=$sql."id LIKE ? and ";
 $sql=$sql."order by ".$_POST['orden'];
 
+$likeNombre= "%".$_POST['nombre']."%";
+$likeApellido= "%".$_POST['apellido']."%";
+$likeFecha= "%".$_POST['fecha']."%";
+$likeInscripto= "%".$_POST['inscripto']."%";
+$likeLegajo= "%".$_POST['legajo']."%";
+$likePromedio= "%".$_POST['promedio']."%";
+$likeFoto= "%".$_POST['foto']."%";
+$likeId= "%".$_POST['id']."%";
 
-if (( $resultado = $mysqli->query($sql))) { //devuelve obj $resultado
-  $resultadoCuentaRegistros = $resultado->num_rows;
-  $articulos=[];
-  while($fila=$resultado->fetch_assoc()) {
-  $objArticulo = new stdClass();
-  $objArticulo->codnombre=$fila['nombre'];
-  $objArticulo->codapellido=$fila['apellido'];
-  $objArticulo->codfecha=$fila['Fecha'];
-  $objArticulo->codinscripto=$fila['inscripto'];
-  $objArticulo->codlegajo=$fila['legajo'];
-  $objArticulo->codpromedio=$fila['Promedio'];
-  $objArticulo->codfoto=$fila['foto-carnet'];
-  $objArticulo->codid=$fila['id'];
-  array_push($articulos,$objArticulo);
-}
-  $objArticulos = new stdClass();
-  $objArticulos->usuario=$articulos;
-  $objArticulos->cuenta=$resultadoCuentaRegistros;
-  $salidaJson = json_encode($objArticulos);
-  $mysqli->close();
-  echo $salidaJson;
 
-  die();
+if (( $resultado = $mysqli->prepare($sql))) { //devuelve obj $resultado
+  if($resultado->bind_param("ssssssss", $likeNombre, $likeApellido, $likeFecha, $likeInscripto, $likeLegajo, $likePromedio, $likeFoto, $likeId)){
+    if($resultado->execute()){
+      $resultadoCuentaRegistros = $resultado->num_rows;
+      $articulos=[];
+      while($fila=$resultado->fetch_assoc()) {
+      $objArticulo = new stdClass();
+      $objArticulo->codnombre=$fila['nombre'];
+      $objArticulo->codapellido=$fila['apellido'];
+      $objArticulo->codfecha=$fila['Fecha'];
+      $objArticulo->codinscripto=$fila['inscripto'];
+      $objArticulo->codlegajo=$fila['legajo'];
+      $objArticulo->codpromedio=$fila['Promedio'];
+      $objArticulo->codfoto=$fila['foto-carnet'];
+      $objArticulo->codid=$fila['id'];
+      array_push($articulos,$objArticulo);
+      }
+      $objArticulos = new stdClass();
+      $objArticulos->usuario=$articulos;
+      $objArticulos->cuenta=$resultadoCuentaRegistros;
+      $salidaJson = json_encode($objArticulos);
+      $mysqli->close();
+      echo $salidaJson;
+
+      die();
+    }
+  }
 }
  ?>
